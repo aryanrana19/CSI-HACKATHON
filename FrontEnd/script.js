81b3d6aeaba1a532a1efa6bdb90b6a7d6731ebb1
Aryan Rana <itsaryanrana@gmail.com>
2024-03-17 00:07:18 +0530

    card and nav

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,8 +2,19 @@ let popup = document.getElementById("popup")
 
 function openPopup(){
     popup.classList.add("open-popup")
+    document.addEventListener("click", handleClickOutside);
 }
 
 function closePopup(){
     popup.classList.remove("open-popup")
+
 }
+
+
+const budgetSlider = document.getElementById("budget");
+
+const budgetValue = document.getElementById("budgetValue");
+
+budgetSlider.addEventListener("input", function() {
+  budgetValue.textContent = budgetSlider.value;
+});
